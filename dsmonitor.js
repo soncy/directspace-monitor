edd38b3ee41a6bf34e8eb855f7ecbf147a735143
soncy <cs1900@gmail.com>
2013-05-01 11:06:37 +0800

    for test

--- dsmonitor.js
+++ dsmonitor.js
@@ -18,7 +18,7 @@ var URL = 'https://eportal.directspace.net/cart.php?gid=22',
 var arguments = process.argv,
     currUser = hostName = sendEmailAdress= null,
     email = arguments[2] || DEFAULTEMAIL,
-    checkTime = 10, //单位：秒
+    checkTime = 1, //单位：秒
     test = arguments[3],
     tested = false;
 
