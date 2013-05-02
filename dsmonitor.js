943f469c3739d7e8f043b90eede957b5870ce6f2
soncy <cs1900@gmail.com>
2013-05-02 16:51:35 +0800

    update dsmonitor.js

--- dsmonitor.js
+++ dsmonitor.js
@@ -5,39 +5,47 @@
  * Time: 21:47:44
  * Contact: http://www.qifendi.com
  */
-var https = require('https'),
+var https      = require('https'),
     nodemailer = require('nodemailer'),
-    exec = require('child_process').exec;
+    exec       = require('child_process').exec;
 
-var URL = 'https://eportal.directspace.net/cart.php?gid=22',
+var URL          = 'https://eportal.directspace.net/cart.php?gid=22',
     DEFAULTEMAIL = 'soncy1986@gmail.com',
-    DEFAULTUSER = 'directspace',
-    DEFULTHOST = 'directspace.net';
+    DEFAULTUSER  = 'directspace',
+    DEFULTHOST   = 'directspace.net';
 
-var arguments = process.argv,
-    currUser = hostName = sendEmailAdress= null,
-    email = arguments[2] || DEFAULTEMAIL,
-    checkTime = 120, //单位：秒
-    test = arguments[3],
-    tested = false;
+var arguments   = process.argv,
+    currUser    = hostName = sendEmailAdress = null,
+    email       = arguments[2] || DEFAULTEMAIL,
+    checkTime   = 120, //单位：秒
+    test        = arguments[3],
+    regForType  = /(.*?)DSVPS\.1\<\/strong\>(.*?)\<strong(.*?)/,
+    regForCount = /(.*?)\((.*?) Available(.*?)/;
+    
 
-function __istest__() {
-    if (tested) return false;
-
-    if (test && (test === '-t' || test === '--test' )) {
-        return true;
-    }
-
-    if (!test && (email === '-t' || email === '--test')) {
-        email = DEFAULTEMAIL;
-        return true;
-    }
-
+function DSMonitor() {
+    this.tested = false;
 }
 
-function getSourceCode(callback) {
-    var content = '';
-    var req = https.get(URL, function(res) {
+DSMonitor.prototype.start = function() {
+    var self = this;
+
+    if (self._istest()) {
+        self._sendEmail();
+        log("发送测试邮件");
+        self.tested = true;
+    }
+
+    self._getSourceCode(function(data) {
+        self._findSales(data);
+    });
+}
+
+DSMonitor.prototype._getSourceCode = function(callback) {
+    var content = '',
+        self = this;
+
+    https.get(URL, function(res) {
         res.on('data', function(d) {
             content += d.toString();
         });
@@ -45,48 +53,42 @@ function getSourceCode(callback) {
             callback(content);
         });
     }).on('error', function(e) {
-        recheck();
+        self._recheck();
     });
-    req = null;
+    
 }
 
-function findSales(data) {
-    var reg = /(.*?)DSVPS\.1\<\/strong\>(.*?)\<strong(.*?)/,
-        count = reg.exec(data),
+DSMonitor.prototype._findSales = function(data) {
+    var self = this,
+        count = regForType.exec(data),
         regString = count[2];
 
     if (~regString.indexOf('em')) {
-        var c = /(.*?)\((.*?) Available(.*?)/.exec(regString);
+        var c = regForCount.exec(regString);
         if (c && parseInt(c[2]) < 1) {
-            recheck();
+            self._recheck();
             return;
         } 
     }
-    available();
+    self._available();
 }
 
-function available() {
-    console.log(nowDate() + ' ======== 放货了，已发送邮件到:' + email +' =======');
-    sendEmail();
+DSMonitor.prototype._recheck = function() {
+    var self = this;
+    log(nowDate() + ':本次检查没有放货，' + checkTime + '秒后再次检查');
+    setTimeout(function() {
+        self.start.call(this);
+    }, checkTime * 1000); // 循环检查
 }
 
-function recheck() {
-    console.log(nowDate() + ' ======== 本次检查没有放货，' + checkTime + '秒后再次检查 =======');
-    setTimeout(start, 1 * checkTime * 1000); // 循环检查
+DSMonitor.prototype._available = function() {
+    var self = this;
+    log(nowDate() + ':放货了，已发送邮件到:' + email);
+    self._sendEmail();
 }
 
-function nowDate() {
-    var date = new Date(),
-        year = date.getFullYear(),
-        month = date.getMonth(),
-        day = date.getDate(),
-        hour = date.getHours(),
-        minute = date.getMinutes();
 
-    return [year, month, day].join('-') + ' ' + [hour, minute].join(':');
-}
-
-function sendEmail() {
+DSMonitor.prototype._sendEmail = function() {
     nodemailer.SMTP = {
         host: 'localhost'
     };
@@ -98,60 +100,85 @@ function sendEmail() {
         html: '<a href="' + URL + '">Buy</a>',
         body: 'DirectSpace 有货啦！' + URL
     }, function(error, success){
-        console.log('发送到' + email + '： ' + (error ? '失败，检查sendmail是否安装并启动' : '成功'));
+        log('发送到' + email + '： ' + (error ? '失败，检查sendmail是否安装并启动' : '成功'));
     });
+}
+
+DSMonitor.prototype._istest = function() {
+    var self = this;
+
+    if (self.tested) return false;
+
+    // 如果有第三个参数，则判断第三个参数
+    if (test && hasTestArgument(test)) {
+        return true;
+    }
+
+    /* 如果没有第三个参数，则判断第二个参数：email
+     * node dsmonitor.js -t 等同于 node dsmonitor DEFAULTEMAIL -t
+     */
+    if (!test && hasTestArgument(email)) {
+        email = DEFAULTEMAIL;
+        return true;
+    }
+}
+
+function hasTestArgument(val) {
+    return (val === '-t' || val === '--test');
+}
+
+function log(val) {
+    console.log('========== ' + val + ' =============');
+}
+
+function nowDate() {
+    var date   = new Date(),
+        year   = date.getFullYear(),
+        month  = date.getMonth(),
+        day    = date.getDate(),
+        hour   = date.getHours(),
+        minute = date.getMinutes();
+
+    return [year, month, day].join('-') + ' ' + [hour, minute].join(':');
 }
 
 function monitoring() {
+
+    // 获取当前登录用户
     execSystemCommand('echo `whoami`', function(stdout) {
         currUser = stdout;
         getSendEmailAdress(function() {
-            start();
+            dsmonitor.start();
         });
     });
     
+    // 获取当前hostname
     execSystemCommand('hostname', function(stdout) {
         hostName = stdout;
         getSendEmailAdress(function() {
-            start();
+            dsmonitor.start();
         });
     });
+
 }
 
 function execSystemCommand(command, callback) {
-    var a = null;
     try{
-        a = exec(command, function(error, stdout, stderr) {
+        exec(command, function(error, stdout, stderr) {
             callback(stdout);
             if (error !== null) {
                 console.log('exec error: ' + error);
                 setDefault();
             }
-            a.kill();
         });
     } catch (e) {
         setDefault();
     }
-
-    return a;
 }
 
 function setDefault() {
     currUser = DEFAULTUSER;
     hostName = DEFULTHOST;
-}
-
-function start() {
-    if (__istest__()) {
-        sendEmail();
-        console.log("======= 发送测试邮件 =========");
-        tested = true;
-    }
-
-    getSourceCode(function(data) {
-        findSales(data);
-        data = null;
-    });
 }
 
 function getSendEmailAdress(callback) {
@@ -161,4 +188,6 @@ function getSendEmailAdress(callback) {
     }
 }
 
+var dsmonitor = new DSMonitor();
 monitoring();
+
