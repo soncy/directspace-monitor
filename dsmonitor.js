63dba92a41ac82efc5d08f7f6c0ef8b7f7a8ddc5
soncy <cs1900@gmail.com>
2013-05-03 22:00:31 +0800

    完善测试功能

--- dsmonitor.js
+++ dsmonitor.js
@@ -22,7 +22,7 @@ var conf = {
     url       : "https://eportal.directspace.net/cart.php?gid=22",
     email     : arguments[2] || DEFAULTEMAIL,
     checkTime : 120, //检查间隔，单位：秒
-    reg       : [/(.*?)DSVPS\.3\<\/strong\>(.*?)\<strong(.*?)/, /(.*?)\((.*?) Available(.*?)/]
+    reg       : [/(.*?)DSVPS\.1\<\/strong\>(.*?)\<strong(.*?)/, /(.*?)\((.*?) Available(.*?)/]
 };
 
 
